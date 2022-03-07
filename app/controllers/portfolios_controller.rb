7e6462b0fa4bf16f4b2ab844260692cace34b0a6
MayurakshiGoel <mayurakshi@yabx.co>
2022-03-07 11:30:31 +0530

    Lecture 60

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,22 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolios_items = Portfolio.all
 	end
+
+	def new
+		@portfolios_items = Portfolio.new
+	end
+
+
+	def create
+    @portfolios_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolios_items.save
+        format.html { redirect_to portfolios_path, notice: "Your portfolio is now live" }
+      else
+        format.html { render :new}
+      end
+    end
+  end
+
 end
